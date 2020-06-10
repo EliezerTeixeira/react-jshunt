9888f09d525f6d7db0a3c675b2d2262dc34e3a65
EliezerTeixeira <eliezerteixeira@outlook.pt>
2020-06-10 15:01:04 +0100

    prev and next page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,7 +5,9 @@ import './style.css'
 
 export default class Main extends Component{
     state = {
-        products: []
+        products: [],
+        productInfo: {},
+        page: 1
     }
 
 
@@ -13,14 +15,36 @@ export default class Main extends Component{
         this.loadProducts()
     }
 
-    loadProducts = async () =>{
-        const response = await api.get('/products')
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/products?page=${page}`)
 
-        this.setState({ products: response.data.docs })
+        const { docs, ...productInfo } = response.data
+
+        this.setState({ products: docs, productInfo, page })
+    }
+
+    prevPage = () =>{
+        const { page } = this.state
+
+        if(page === 1) return
+
+        const pageNumber = page - 1
+
+        this.loadProducts(pageNumber)
+    }
+
+    nextPage = () =>{
+        const { page, productInfo } = this.state
+
+        if(page === productInfo.pages) return
+
+        const pageNumber = page + 1
+
+        this.loadProducts(pageNumber)
     }
 
     render(){
-        const { products } = this.state
+        const { products, page, productInfo } = this.state
 
         return (
             <div className="product-list">
@@ -31,7 +55,14 @@ export default class Main extends Component{
                         <a href="">Acessar</a>
                     </article>
                 ))}
+
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+                </div>
+
             </div>
+
         )
     }
 }
